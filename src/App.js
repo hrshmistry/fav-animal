9a0993f9529984b18dea7bd3edd7a1411fb32abc
Harsh Mistry <47841863+hrshmistry@users.noreply.github.com>
2021-01-21 18:13:52 +0530

    upto username prompt+

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./styles.css";
 
+var color = "blue";
+var username = prompt("Give me your username!");
+
 export default function App() {
-  function LikeClickHandler() {
-    console.log("clicked.");
-  }
+  // function LikeClickHandler() {
+  //   console.log("clicked.");
+  // }
 
   return (
     <div className="App">
-      <h1>inside out</h1>
-      <button onClick={LikeClickHandler}>Like me!</button>
+      <h1>
+        inside out <span style={{ color: color }}> {username} </span>
+      </h1>
+      {/* <button onClick={LikeClickHandler}>Like me!</button> */}
     </div>
   );
 }
