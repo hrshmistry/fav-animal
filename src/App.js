983486f660776209d8ac2a2db0f8a183a66fd5fd
Harsh Mistry <47841863+hrshmistry@users.noreply.github.com>
2021-01-21 21:32:36 +0530

    click handler and change handler with useState()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+
+var emojiWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
-  const [userInput, setUserInput] = useState("");
-  function onChangeHandler(event) {
-    console.log(event.target.value);
-    setUserInput(event.target.value);
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We don't have this in our DB";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
   }
 
   return (
     <div className="App">
       <h1>inside out</h1>
-      <input onChange={onChangeHandler}></input>
-      <div>Welcome {userInput}</div>
+      <input onChange={emojiInputHandler}></input>
+      <h2>{meaning}</h2>
+      <h3>emojis we know</h3>{" "}
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-/*
-React is "VISER"
-View -> Interect -> State in Event handler -> React
-*/
